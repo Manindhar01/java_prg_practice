b567ecc975a291bccf61a0ae7b634d6d2d4cae47
Manindhar1 <manintharvlr59@gmail.com>
2024-02-20 12:31:45 +0530

    added factorial program

---
+++ java_prg/src/java_prg/Factorial_1.java
@@ -0,0 +1,16 @@
+package java_prg;
+
+public class Factorial_1 {
+
+	public static void main(String[] args) {
+				int n=8;
+				long fact=1;
+				for(int i=1;i<=n;i++){
+					fact*=i;
+				}
+				System.out.println("Factorial of 8 is:"+fact);
+
+
+	}
+
+}
