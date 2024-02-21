8ad1716b11b80d58fcca926932859798e80d76e7
Manindhar1 <manintharvlr59@gmail.com>
2024-02-21 18:12:00 +0530

    commited prg

--- java_prg/src/java_prg/Fibonacci_Whileloop_6.java
+++ java_prg/src/java_prg/Fibonacci_Whileloop_6.java
@@ -4,6 +4,16 @@ public class Fibonacci_Whileloop_6 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		int f1=0,f2=1,f3;
+		int i=1;
+		System.out.println(f1+" "+f2);
+		while(i<=10) {
+			f3=f2+f1;
+			System.out.println(" "+f3);
+			f1=f2;
+			f2=f3;
+			i++;
+		}
 
 	}
 
